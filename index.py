2373dc7ef43f4470f3ad6c4d91156c60e893cc6d
waldo237 <waldo23792@gmail.com>
2021-04-20 23:00:18 -0400

    added file walker

--- index.py
+++ index.py
@@ -1,13 +1,23 @@
-import sys
+# import sys
 
-class Square:
-    def __init__(self, length, width) -> None:
-        self.length = length
-        self.width = width
-    def area(self):
-        return self.width * self.length
+# class Square:
+#     def __init__(self, length, width) -> None:
+#         self.length = length
+#         self.width = width
+#     def area(self):
+#         return self.width * self.length
     
-r = Square(43, 54)
-print("square area: %dft" %(r.area()))
+# r = Square(43, 54)
+# print("square area: %dft" %(r.area()))
 
-print(sys)
+# print(sys)
+import os
+
+for folder_name, subfolders, filenames in os.walk('C:\\Users'):
+    print('The current folder is {}'.format(folder_name))
+    for subfolder in subfolders:
+            print('SUBFOLDER OF {}: {}'.format(folder_name, subfolder))
+            for filename in filenames:
+                print('FILE INSIDE {}: {}'.format(folder_name, filename))
+
+            print('')
