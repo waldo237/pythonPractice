222c8518501c8816e45970f8d170b325c6d46d25
waldo237 <waldo23792@gmail.com>
2021-05-04 06:24:00 -0400

    added insert at head

--- dll.py
+++ dll.py
@@ -1,3 +1,6 @@
+from typing import Counter
+
+
 class Node(object):
     """ A node for a doublyLinkedList """
 
@@ -66,3 +69,36 @@ class DoublyLinkedList(object):
                     current.next.prev = current.prev
                     node_deleted = True
                 current = current.next
+
+        if node_deleted:
+            self.count -= 1
+
+    def search(self, data):
+        """ Search through the list. Return True if found """
+        for node in self.iter():
+            if data == data:
+                return True
+        return False
+
+    def print_forward(self):
+        """ Print the nodes in the list from beggining to end """
+        for node in self.iter():
+            print(node)
+
+    def print_backward(self):
+        """ Print from last to first. """
+        current = self.tail
+        while current:
+            print(current.data)
+            current = current.prev
+
+    def insert_head(self, data):
+        """ Insert a  new node at the head of the list """
+        if self.head is not None:
+            new_node = Node(data, None, None)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            self.count+= 1
+
+    
