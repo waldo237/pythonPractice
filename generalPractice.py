d239e2d3697ea87ffa65bfa7de8dc5f63175d061
waldo237 <waldo23792@gmail.com>
2021-05-09 15:14:51 -0400

    practiced lambdas

--- generalPractice.py
+++ generalPractice.py
@@ -1,53 +1,22 @@
 
 
-def filterFunc(x):
-    if x % 2 == 0:
-        return False
-    return True
+def CelsisusToFahrenheit(temp):
+    return (temp * 9/5) + 32
 
 
-def filter2(x):
-    if x.isupper():
-        return False
-    return True
-
-
-def sqFunc(x):
-    return x**2
-
-
-def toGrade(x):
-    if x >= 90:
-        return "A"
-    elif x >= 80 and x < 90:
-        return "B"
-    elif x >= 70 and x < 80:
-        return "C"
-    elif x >= 65 and x < 70:
-        return "D"
-    return "F"
+def FahrenheitToCelsisus(temp):
+    return (temp-32) * 5/9
 
 
 def main():
-    nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
-    chars = "abcDeFGHiJklmnoP"
-    grades = (81, 89, 94, 78, 61, 66, 99, 74)
+    ctemps = [0, 12, 34, 100]
+    ftemps = [32, 65, 100, 212]
 
-    odds = list(filter(filterFunc, nums))
-    even = list(filter(lambda t: not filterFunc(t), nums))
-    print('odds', odds)
-    print('even numbers', even)
+    print(list(map(FahrenheitToCelsisus, ftemps)))
+    print(list(map(CelsisusToFahrenheit, ctemps)))
 
-    lowers = list(filter(filter2, chars))
-    upper = list(filter(lambda t: not filter2(t), chars))
-    print(lowers)
-    print(upper)
-
-    squares = list(map(sqFunc, nums))
-    print(squares)
-
-    letters = list(map(toGrade, grades))
-    print(letters)
+    print(list(map(lambda t:(t-32)* 5/9, ftemps)))
+    print(list(map(lambda t:(t * 9/5) + 32, ftemps)))
 
 
 if __name__ == "__main__":
