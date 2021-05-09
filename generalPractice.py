5c6fb50c0d7f102a9c2593021505842121a24620
waldo237 <waldo23792@gmail.com>
2021-05-09 16:32:33 -0400

    overiding default methods

--- generalPractice.py
+++ generalPractice.py
@@ -1,26 +1,48 @@
-from collections import defaultdict
+
+class Employee():
+    def __init__(self, fname, lname, level, yrsService):
+        self.fname = fname
+        self.lname = lname
+        self.level = level
+        self.seniority = yrsService
+
+    def __ge__(self, other):
+        if(self.level == other.level):
+            return self.seniority >= other.seniority
+        return self.level >= other.level
+
+    def __gt__(self, other):
+        if self.level == other.level:
+            return self.seniority > other.seniority
+        return self.level > other.level
+
+    def __lt__(self, other):
+        if self.level == other.level:
+            return self.seniority < other.seniority
+        return self.level < other.level
+
+    def __le__(self, other):
+        if(self.level == other.level):
+            return self.seniority <= other.seniority
+        return self.level <= other.level
+
+    def __eq__(self, other):
+        return self.level == other.level
 
 
 def main():
-    fruits = ['mango', 'peach', 'orange', 'banana',
-        'apple', 'grape', 'banana', 'banana', 'mango']
+    dept = []
+    dept.append(Employee("Tim", "Sims", 5, 9))
+    dept.append(Employee("John", "Doe", 4, 12))
+    dept.append(Employee("Jane", "Smith", 6, 6))
+    dept.append(Employee("Rebecca", "Robinson", 5, 13))
+    dept.append(Employee("Tyler", "Durden", 5, 12))
 
-    fruitCounter = defaultdict(int)
+    print(bool(dept[0] > dept[2]))
+    print(bool(dept[4] < dept[3]))
+    emps = sorted(dept)
+    for emp in emps:
+        print(emp.lname)
 
-    for fruit in fruits:
-        fruitCounter[fruit] += 1
-
-    for(k,v) in fruitCounter.items():
-        print(f"{k}: {str(v)}")
-        
 if __name__ == "__main__":
     main()
-
-""" 
-mango: 2
-peach: 1
-orange: 1
-banana: 3
-apple: 1
-grape: 1
- """
