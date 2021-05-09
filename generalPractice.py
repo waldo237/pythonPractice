002994f9c31e13f61e94639667a1ffd978c7827d
waldo237 <waldo23792@gmail.com>
2021-05-09 14:57:20 -0400

    practiced filter and map

--- generalPractice.py
+++ generalPractice.py
@@ -1,29 +1,53 @@
-import itertools
 
 
-def testFunction(x):
-    return x < 50
+def filterFunc(x):
+    if x % 2 == 0:
+        return False
+    return True
+
+
+def filter2(x):
+    if x.isupper():
+        return False
+    return True
+
+
+def sqFunc(x):
+    return x**2
+
+
+def toGrade(x):
+    if x >= 90:
+        return "A"
+    elif x >= 80 and x < 90:
+        return "B"
+    elif x >= 70 and x < 80:
+        return "C"
+    elif x >= 65 and x < 70:
+        return "D"
+    return "F"
+
 
 def main():
-    seq1 = ["Joe", "Juan", "Marlon"]
-    cycle1 = itertools.cycle(seq1)
-    print(next(cycle1))
-    print(next(cycle1))
-    print(next(cycle1))
+    nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
+    chars = "abcDeFGHiJklmnoP"
+    grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
-    count1 = itertools.count(100, 10)
-    print(next(count1))
-    print(next(count1))
-    print(next(count1))
+    odds = list(filter(filterFunc, nums))
+    even = list(filter(lambda t: not filterFunc(t), nums))
+    print('odds', odds)
+    print('even numbers', even)
 
-    vals = [10, 20, 30, 40, 50, 40, 30]
-    acc = itertools.accumulate(vals, min)
-    print(list(acc))
-    x = itertools.chain('ABCD', '1234')
-    print(list(x))
-    
-    print(list(itertools.dropwhile(testFunction, vals)))
-    print(list(itertools.takewhile(testFunction, vals)))
+    lowers = list(filter(filter2, chars))
+    upper = list(filter(lambda t: not filter2(t), chars))
+    print(lowers)
+    print(upper)
+
+    squares = list(map(sqFunc, nums))
+    print(squares)
+
+    letters = list(map(toGrade, grades))
+    print(letters)
 
 
 if __name__ == "__main__":
