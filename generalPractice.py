918b8952907bc192658537693f370d49e89c7208
waldo237 <waldo23792@gmail.com>
2021-05-09 15:19:43 -0400

    practiced var arguments

--- generalPractice.py
+++ generalPractice.py
@@ -1,23 +1,16 @@
+def addition(base, *args):
+    result = 0
+    for arg in args:
+        result += arg
 
-
-def CelsisusToFahrenheit(temp):
-    return (temp * 9/5) + 32
-
-
-def FahrenheitToCelsisus(temp):
-    return (temp-32) * 5/9
+    return result
 
 
 def main():
-    ctemps = [0, 12, 34, 100]
-    ftemps = [32, 65, 100, 212]
-
-    print(list(map(FahrenheitToCelsisus, ftemps)))
-    print(list(map(CelsisusToFahrenheit, ctemps)))
-
-    print(list(map(lambda t:(t-32)* 5/9, ftemps)))
-    print(list(map(lambda t:(t * 9/5) + 32, ftemps)))
-
+    print(addition(43,24,65 *87,5456, 23,4,5.5))
+    print(addition(43,24,65,87,5456, 23,4,5.5))
+    myNums = [5, 10, 15, 20]
+    print(addition(*myNums))
 
 if __name__ == "__main__":
     main()
