f97f407abf441f7b175fbe432ed854f7059539a6
waldo237 <waldo23792@gmail.com>
2021-05-09 17:13:18 -0400

    customized string representations of objects

--- generalPractice.py
+++ generalPractice.py
@@ -1,48 +1,37 @@
+class Color():
+    def __init__(self):
+        self.red = 50
+        self.green = 75
+        self.blue = 100
 
-class Employee():
-    def __init__(self, fname, lname, level, yrsService):
-        self.fname = fname
-        self.lname = lname
-        self.level = level
-        self.seniority = yrsService
+    def __getattr__(self, attr):
+        if attr == "rgbcolor":
+            return f"rgb({self.red},{ self.green}, {self.blue})"
+        elif attr == "hexcolor":
+            return f"#{self.red:02x}{self.green:02x}{self.blue:02x}"
+        else:
+            raise AttributeError
 
-    def __ge__(self, other):
-        if(self.level == other.level):
-            return self.seniority >= other.seniority
-        return self.level >= other.level
+    def __setattr__(self, attr, value):
+        if attr == "rgbcolor":
+            self.red = value[0]
+            self.green = value[1]
+            self.blue = value[2]
+        else:
+            super().__setattr__(attr, value)
 
-    def __gt__(self, other):
-        if self.level == other.level:
-            return self.seniority > other.seniority
-        return self.level > other.level
+    def __dir__(self):
+        return ("rgbcolor", "hexcolor")
 
-    def __lt__(self, other):
-        if self.level == other.level:
-            return self.seniority < other.seniority
-        return self.level < other.level
+def main ():
+    cls1 = Color()
+    print(cls1.rgbcolor)
+    print(cls1.hexcolor)
+    cls1.rgbcolor = (125, 200, 86)
+    print(cls1.rgbcolor)
+    print(cls1.hexcolor)
 
-    def __le__(self, other):
-        if(self.level == other.level):
-            return self.seniority <= other.seniority
-        return self.level <= other.level
-
-    def __eq__(self, other):
-        return self.level == other.level
-
-
-def main():
-    dept = []
-    dept.append(Employee("Tim", "Sims", 5, 9))
-    dept.append(Employee("John", "Doe", 4, 12))
-    dept.append(Employee("Jane", "Smith", 6, 6))
-    dept.append(Employee("Rebecca", "Robinson", 5, 13))
-    dept.append(Employee("Tyler", "Durden", 5, 12))
-
-    print(bool(dept[0] > dept[2]))
-    print(bool(dept[4] < dept[3]))
-    emps = sorted(dept)
-    for emp in emps:
-        print(emp.lname)
-
+    # access a regular attribute
+    print(cls1.red)
 if __name__ == "__main__":
     main()
