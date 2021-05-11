f2dcc60e0d38a2adfc006bca1fe9b10fd2d6910e
waldo237 <waldo23792@gmail.com>
2021-05-11 06:50:50 -0400

    practice itertools

--- generalPractice.py
+++ generalPractice.py
@@ -1,27 +1,15 @@
-import datetime
-
-
-def perm(l):
-    if len(l) <= 1:
-        return [l]
-    r = []
-    for i in range(len(l)):
-        s = l[:i] + l[i + 1:]
-        p = perm(s)
-        for x in p:
-            r.append(l[i: i + 1] + x)
-    return r
-
+""" itertools.compress() Filters one iterable with another."""
+import itertools
 
 def main():
-    evens = [2, 4, 6, 8]
-    res = perm(evens)
-    name = "Fred"
-    print(len("He said his name is {name!r}."))
-
-    today = datetime.date(year=2017, month=1, day=27)
-    print(f"{today:%B %d, %Y}")
+    class1 = ["Bob", "James", "Chad", "Darcy", "Penny", "Hannah"
+            "Kevin", "James", "Melanie", "Becky", "Steve", "Frank"]
+    selections = [True, False, True, False, True]
+    result = itertools.compress(class1, selections)
+    for each in result:
+        print(each, end=" ")
 
 
 if __name__ == "__main__":
     main()
+""" Bob Chad Penny """
