abc1fee0a164e0a02db9e5dda8d6dd87df15f817
waldo237 <waldo23792@gmail.com>
2021-06-02 21:08:00 -0400

    started practices

--- generalPractice.py
+++ generalPractice.py
@@ -1,9 +1,10 @@
 """ itertools.compress() Filters one iterable with another."""
 import itertools
 
+
 def main():
     class1 = ["Bob", "James", "Chad", "Darcy", "Penny", "Hannah"
-            "Kevin", "James", "Melanie", "Becky", "Steve", "Frank"]
+              "Kevin", "James", "Melanie", "Becky", "Steve", "Frank"]
     selections = [True, False, True, False, True]
     result = itertools.compress(class1, selections)
     for each in result:
