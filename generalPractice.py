3a30600c57c17a9b63dd762364f84122c836ea19
waldo237 <waldo23792@gmail.com>
2021-06-03 19:36:12 -0400

    triangle

--- generalPractice.py
+++ generalPractice.py
@@ -2,15 +2,30 @@
 import itertools
 
 
-def main():
-    class1 = ["Bob", "James", "Chad", "Darcy", "Penny", "Hannah"
-              "Kevin", "James", "Melanie", "Becky", "Steve", "Frank"]
-    selections = [True, False, True, False, True]
-    result = itertools.compress(class1, selections)
-    for each in result:
-        print(each, end=" ")
+# def main():
+#     class1 = ["Bob", "James", "Chad", "Darcy", "Penny", "Hannah"
+#               "Kevin", "James", "Melanie", "Becky", "Steve", "Frank"]
+#     selections = [True, False, True, False, True]
+#     result = itertools.compress(class1, selections)
+#     for each in result:
+#         print(each, end=" ")
 
+
+def triangle(row):
+    for i in range(row):
+        print(' ' * (row-i-1) + '+' * (2 * i + 1))
 
 if __name__ == "__main__":
-    main()
-""" Bob Chad Penny """
+    triangle(12)
+"""        +
+          +++
+         +++++
+        +++++++
+       +++++++++
+      +++++++++++
+     +++++++++++++
+    +++++++++++++++
+   +++++++++++++++++
+  +++++++++++++++++++
+ +++++++++++++++++++++
++++++++++++++++++++++++ """
